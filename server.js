5c83cc01ca49c655f2114870c0eabf261932a8db
Sabrina Hanson <sabhanson7@gmail.com>
2022-02-04 10:37:37 -0800

    starting folder and file setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const routes = require('./routes');
+require('dotenv').config();
+const sequelize = require('./config/connection');
+// import sequelize connection
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({force: false}).then(() => {
+  app.listen(PORT, () => console.log(`Now listening at PORT ${PORT}`));
+});
+
